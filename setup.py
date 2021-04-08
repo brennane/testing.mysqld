3ac8240e407235eb2be34afaf3c00174ecd8c9e3
Brennan Evans <breevans@cisco.com>
2021-04-08 14:30:01 -0500

    remove py2 support

--- setup.py
+++ setup.py
@@ -7,12 +7,10 @@ classifiers = [
     "Intended Audience :: Developers",
     "License :: OSI Approved :: Apache Software License",
     "Programming Language :: Python",
-    "Programming Language :: Python :: 2.6",
-    "Programming Language :: Python :: 2.7",
-    "Programming Language :: Python :: 3.2",
-    "Programming Language :: Python :: 3.3",
-    "Programming Language :: Python :: 3.4",
-    "Programming Language :: Python :: 3.5",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
     "Topic :: Database",
     "Topic :: Software Development",
     "Topic :: Software Development :: Testing",
@@ -20,8 +18,6 @@ classifiers = [
 
 install_requires = ['testing.common.database @ git+https://github.com/brennane/testing.common.database@master#egg=testing.common.database',
                     'pymysql']
-if sys.version_info < (2, 7):
-    install_requires.append('unittest2')
 
 
 setup(
